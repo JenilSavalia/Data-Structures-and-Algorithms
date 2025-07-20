4d2c01fc56d21fa70d0ca4ce9355832ceef1a61e
Jenil Savalia <jenil.savalia.cd@gmail.com>
2025-07-20 10:16:59 +0530

    Max element in Binary Tree

---
+++ 1 Binary Tree/Questions/1.max_element_Binary_tree.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+    
+    public :
+    int data;
+    Node* left;
+    Node* right;
+    
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+// Bullding a binary tree with help of Pre-Ordered Sequence
+
+// Recursive Function to Build Preorder Binaryy Tree
+static int idx = -1;
+Node* buildTree(vector<int> preorder){
+    idx++;
+    
+    if(preorder[idx]==-1){
+        return NULL;
+    }
+    
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder); // LEFT
+    root->right = buildTree(preorder); // RIGHT
+    
+    return root;
+    
+    }
+ 
+
+
+// PreOrder Traversal -> ROOT -> LEFT -> RIGHT (DLR)
+// Time Complexity: O(n)
+// Space Complexity: O(h) where h is the height of the tree
+int maxValue = -1;
+ void preOrder(Node* root){
+     
+
+     if(root==NULL){
+         return;
+     }
+    //  cout << root-> data << endl;
+    if (root->data > maxValue)
+    {
+        maxValue = root->data;
+    }
+     preOrder(root->left);
+     preOrder(root->right);
+     
+ }
+ 
+
+int main() {
+vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+
+Node* root = buildTree(preorder);
+preOrder(root);
+cout << maxValue << endl;
+    return 0;
+}
