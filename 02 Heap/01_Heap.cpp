635770fa932cfb3b1f49b9a8893b2442403a6d12
Jenil Savalia <jenil.savalia.cd@gmail.com>
2025-08-04 10:49:54 +0530

    Intro to Heap

---
+++ 02 Heap/01_Heap.cpp
@@ -0,0 +1,24 @@
+// Heaps are Complete Binary trees
+
+// 1. MAX Heap : 
+// 2. MIN Heap : 
+
+
+
+// Insertion Operation in MAX Heap
+
+// Insert the element at last in the array and compare with its parents and swap elements till condidion of (MAX or MIN heap) is satisfied
+// Time Complexicity for Insertion :  minimum : O(log n) , minimium : O(1)
+
+// in Insertion adjustment is done (Uppward) from down to upward
+
+
+// Delete Operation in MAX Heap
+
+
+// in Deletion adjustment is done (Downward) from upward to downward
+// When we delete a element from Max Heap, we get the largest element (that is root element is deleted) vice versa for MIN Heap
+
+// Time Complexicity for Deletion :  minimum : O(log n) , minimium : O(1)
+
+
