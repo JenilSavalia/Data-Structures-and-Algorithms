5e7646db5f49e0d919081912f230d77cb34ffdfb
Jenil Savalia <jenil.savalia.cd@gmail.com>
2025-09-26 15:17:18 +0530

    Add initial implementation of graph data structure with adjacency list representation

---
+++ 05 Graph/01_Intro.cpp
@@ -0,0 +1,34 @@
+
+// Graph : network of nodes
+
+// edge : connection b/w any two nodes
+// vertices : nodes are called vertex in graph
+
+// Types of Graph :
+
+// BASED on edges //
+
+// 1. uni-directional graph / directed graph
+// edged point to one of the vertex  1->2
+
+// 2. bi-directional graph / undirected graph
+// edged point to both of the vertex  1<->2
+
+// BASED on weight of edges //
+
+// 1. weighted graph
+// 2. un-weughted graph
+
+
+// ------------------
+
+// 1. un-directed un-weighted graph
+// 2. un-direcred weighted graph
+// 3. directed un-weighted graph
+// 3. directed weighted graph
+
+
+
+
+
+
