721f4f95f6e001d8b3d72e5f6c6e5b1f19538ff0
Jenil Savalia <jenil.savalia.cd@gmail.com>
2025-10-17 10:37:57 +0530

    Add implementation to move all zeros to the end of the array

---
+++ 00 Questions/Array/05 Left Rotate the Array by One.cpp
@@ -0,0 +1,30 @@
+
+
+
+// rotateing array by k positions to right;
+
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> temp;
+        int i;
+        if (nums.size() == 1)
+            return;
+        k = k % nums.size();
+        // If k is greater than the length of nums, then you're trying to rotate
+        // the array more times than its length — but logically, rotating an
+        // array by its length (n) results in the same array. So, rotating by k
+        // is the same as rotating by k % n.
+        for (i = nums.size() - k; i < nums.size(); i++) {
+            temp.push_back(nums[i]);
+        }
+        for (i = 0; i < nums.size() - k; i++) {
+            temp.push_back(nums[i]);
+        }
+        for (i = 0; i < nums.size(); i++) {
+            nums[i] = temp[i];
+        }
+        
+    }
+};
---
+++ 00 Questions/Array/06 Move all Zeros to the end.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        if (nums.size() == 1)
+            return;
+        int i;
+        for (i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                // find next non zero element and replace with it
+                for (int j = i+1; j < nums.size(); j++) {
+                    if (nums[j] != 0) {
+                      swap(nums[i],nums[j]);
+                      break;    
+                    }
+                }
+            }
+        }
+
+    }
+};
