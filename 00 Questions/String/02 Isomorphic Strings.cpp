957940614382dd819b093d47e4c8cc0541416029
Jenil Savalia <jenil.savalia.cd@gmail.com>
2025-10-31 17:44:03 +0530

    02 Isomorphic Strings

--- 00 Questions/String/02 Isomorphic Strings.cpp
+++ 00 Questions/String/02 Isomorphic Strings.cpp
@@ -1,37 +1,46 @@
-class Solution {
+class Solution
+{
 public:
-    bool isIsomorphic(string s, string t) {
+    bool isIsomorphic(string s, string t)
+    {
         if (s.length() != t.length())
             return false;
 
-// Isomorphic Strings
-// Each character in s maps to exactly one character in t.
-// And each character in t maps back to exactly one character in s.
+        // Isomorphic Strings
+        // Each character in s maps to exactly one character in t.
+        // And each character in t maps back to exactly one character in s.
 
-// • The positions/order of characters are preserved..
+        // • The positions/order of characters are preserved..
 
-// Each “letter” from the first alphabet must correspond to exactly one unique letter in the second alphabet.
+        // Each “letter” from the first alphabet must correspond to exactly one unique letter in the second alphabet.
 
         map<char, char> s_to_t;
         map<char, char> t_to_s;
 
-        for (int i = 0;i<s.length();i++) {
+        for (int i = 0; i < s.length(); i++)
+        {
             char c1 = s[i];
             char c2 = t[i];
 
             // Check mapping s -> t
-            if (s_to_t.count(c1)) {
+            if (s_to_t.count(c1))
+            {
                 if (s_to_t[c1] != c2)
                     return false;
-            } else {
+            }
+            else
+            {
                 s_to_t[c1] = c2;
             }
 
             // Check mapping t -> s
-            if (t_to_s.count(c2)) {
+            if (t_to_s.count(c2))
+            {
                 if (t_to_s[c2] != c1)
                     return false;
-            } else {
+            }
+            else
+            {
                 t_to_s[c2] = c1;
             }
         }
