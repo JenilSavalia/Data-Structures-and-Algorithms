b8e1aaf263195d33a70685283c347abcaa72766e
Jenil Savalia <jenil.savalia.cd@gmail.com>
2025-11-17 09:58:34 +0530

    MInimum size subarray sum

--- 00 Questions/Array/Sliding Window/02 Minimum Size Subarray Sum.cpp
+++ 00 Questions/Array/Sliding Window/02 Minimum Size Subarray Sum.cpp
@@ -32,3 +32,31 @@
 // When the sum is too small, expanding the window will always increase the sum.
 
 // This allows a two-pointer sliding window approach.
+
+class Solution
+{
+public:
+    int minSubArrayLen(int target, vector<int> &nums)
+    {
+
+        int sum = 0;
+        int length = INT_MAX;
+
+        int start = 0;
+        int end = 0;
+        while (end < nums.size())
+        {
+            sum += nums[end];
+
+            while (sum >= target)
+            {
+                length = min(length, end - start + 1);
+                sum -= nums[start];
+                start++;
+            }
+
+            end++;
+        }
+        return length == INT_MAX ? 0 : length;
+    }
+};
